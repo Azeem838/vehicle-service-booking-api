f3484e22300ba8cce9f84ea7abded03fb8fef3c9
Azeem <azeemahmed101@gmail.com>
2020-10-14 11:03:33 +0200

    add appointment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :service
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :service_id, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
