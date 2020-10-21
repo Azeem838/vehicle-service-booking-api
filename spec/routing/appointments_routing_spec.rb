c6719fb3ae1bfc203cbbdbb7245058b10d7300eb
Azeem <azeemahmed101@gmail.com>
2020-10-21 12:16:07 +0200

    add routing spec

--- spec/routing/appointments_routing_spec.rb
+++ spec/routing/appointments_routing_spec.rb
@@ -6,24 +6,8 @@ RSpec.describe AppointmentsController, type: :routing do
       expect(get: '/appointments').to route_to('appointments#index')
     end
 
-    it 'routes to #show' do
-      expect(get: '/appointments/1').to route_to('appointments#show', id: '1')
-    end
-
     it 'routes to #create' do
       expect(post: '/appointments').to route_to('appointments#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put: '/appointments/1').to route_to('appointments#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch: '/appointments/1').to route_to('appointments#update', id: '1')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete: '/appointments/1').to route_to('appointments#destroy', id: '1')
     end
   end
 end
