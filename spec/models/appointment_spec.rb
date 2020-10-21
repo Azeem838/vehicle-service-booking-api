24b32d9411a91e686d13713b163ccf37d793ab22
Azeem <azeemahmed101@gmail.com>
2020-10-21 12:11:55 +0200

    add spec for models

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:service) }
+
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:service_id) }
+  it { should validate_presence_of(:start_time) }
+  it { should validate_presence_of(:end_time) }
 end
